cf92ab2410582b37da43fb07a901ce364445a8a1
Filipenco Pavel <filipencop@mail.ru>
2020-08-26 22:48:54 +0300

    Changed the 2d vector interface

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,14 @@ mod manifold;
 pub use body::Body;
 pub use world::World;
 
-pub trait VecImpl {
+pub trait VecImpl: std::marker::Copy {
 	fn get_x(&self) -> f32;
 	fn get_y(&self) -> f32;
 	fn set_x(&mut self, x: f32);
 	fn set_y(&mut self, y: f32);
-	fn normalized(&self) -> f32;
+	fn normalized(&self) -> Self;
 	fn plus(&self, other: &Self) -> Self;
 	fn minus(&self, other: &Self) -> Self;
 	fn dot(&self, other: &Self) -> f32;
-	fn zero() -> Self;
+	fn from_xy(x: f32, y: f32) -> Self;
 }
